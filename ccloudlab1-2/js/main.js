788eb622689d29c802f36d43d54d1801772c0f8b
Goni Peles <goni.pl@gmail.com>
2018-05-13 02:06:32 +0200

    Add files via upload

--- ccloudlab1-2/js/main.js
+++ ccloudlab1-2/js/main.js
@@ -57,71 +57,73 @@ function main() {
     aux.loadMedia(0, 19, "ccloudlab1-2/", true, function init() {
         GUI.clear();
         
-        window.alert("Use a USB triple foot switch or 1, 2, and 3 on the keyboard to interact with the scores.");
+        setTimeout(function waitForBrowser() {
+            window.alert("Use a USB triple foot switch or 1, 2, and 3 on the keyboard to interact with the scores.");
 
-        do {
-            performer.index = parseInt(aux.choosePerformer());
+            do {
+                performer.index = parseInt(aux.choosePerformer());
 
-            if (performer.index) {
-                performer.index -= 1;
+                if (performer.index) {
+                    performer.index -= 1;
 
-                do {
-                    performer.current_event = {};
+                    do {
+                        performer.current_event = {};
 
-                    performer.current_event.score = aux.chooseScore(performer.index);
+                        performer.current_event.score = aux.chooseScore(performer.index);
 
-                    if (performer.current_event.score) {
-                        performer.current_event.score -= 1;
-                        
-                        if (performer.current_event.score === 19) { // zr1tS
-                            performer.current_event = {score: 19, part: 0};
-                        } else if (performer.current_event.score !== 15) { // diagram3x1
-                            do {
-                                performer.current_event.part = aux.chooseBeginning(performer.current_event.score);
+                        if (performer.current_event.score) {
+                            performer.current_event.score -= 1;
 
-                                if (performer.current_event.part) {
-                                    performer.current_event.part -= 1;
-                                } else {
-                                    delete performer.current_event;
-                                    break;
-                                }
-                            } while (!performer.current_event);
+                            if (performer.current_event.score === 19) { // zr1tS
+                                performer.current_event = {score: 19, part: 0};
+                            } else if (performer.current_event.score !== 15) { // diagram3x1
+                                do {
+                                    performer.current_event.part = aux.chooseBeginning(performer.current_event.score);
+
+                                    if (performer.current_event.part) {
+                                        performer.current_event.part -= 1;
+                                    } else {
+                                        delete performer.current_event;
+                                        break;
+                                    }
+                                } while (!performer.current_event);
+                            }
+                        } else {
+                            delete performer.current_event;
+                            break;
                         }
-                    } else {
-                        delete performer.current_event;
-                        break;
-                    }
-                } while (!performer.current_event);
-            } else {
-                break;
+                    } while (!performer.current_event);
+                } else {
+                    break;
+                }
+            } while (!performer.current_event);
+
+            if (!performer.current_event) {
+                return;
             }
-        } while (!performer.current_event);
 
-        if (!performer.current_event) {
-            return;
-        }
+            if (score_types[scores[performer.current_event.score].type].formatEvent) {
+                score_types[scores[performer.current_event.score].type].formatEvent(performer, "current_event");
+            }
 
-        if (score_types[scores[performer.current_event.score].type].formatEvent) {
-            score_types[scores[performer.current_event.score].type].formatEvent(performer, "current_event");
-        }
-        
-        if (score_types[scores[performer.current_event.score].type].updateNextEvents) {
-            score_types[scores[performer.current_event.score].type].updateNextEvents(performer);
-        }
+            if (score_types[scores[performer.current_event.score].type].updateNextEvents) {
+                score_types[scores[performer.current_event.score].type].updateNextEvents(performer);
+            }
 
-        if (score_types[scores[performer.current_event.score].type].next_event_index && performer.current_event.part < 11) {
-            performer.next_event_index = 1;
-        } else {
-            performer.next_event_index = 0;
-        }
+            if (score_types[scores[performer.current_event.score].type].next_event_index && performer.current_event.part < 11) {
+                performer.next_event_index = 1;
+            } else {
+                performer.next_event_index = 0;
+            }
 
-        performer.clock = {master: 0, delta: 0, t0: 0};
-        performer.state = "ready";
+            performer.clock = {master: 0, delta: 0, t0: 0};
+            performer.state = "ready";
 
-        GUI.init();
+            GUI.init();
 
-        window.requestAnimationFrame(loop);
+            window.requestAnimationFrame(loop);
 
-        window.onkeyup = interact;
+            window.onkeyup = interact;
+        }, 500);
     });
 }
